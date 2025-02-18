ebacb7a366d3dbac241a6cfe663f6db661350387
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2025-02-18 13:32:07 +0700

    chore: init

---
+++ src/main/java/com/ardwiinoo/springminio/SpringminioApplication.java
@@ -0,0 +1,13 @@
+package com.ardwiinoo.springminio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringminioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringminioApplication.class, args);
+	}
+
+}
